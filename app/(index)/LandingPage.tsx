12b655e4d11a5513521d8e6c58770ed87d9319c3
jun yan  wu <bayoh807@gmail.com>
2024-09-08 02:35:37 +0800

    fix: modify landing page

--- app/(index)/LandingPage.tsx
+++ app/(index)/LandingPage.tsx
@@ -3,22 +3,16 @@ import React from 'react';
 
 const LandingPage: React.FC = () => {
     return (
-        <div className="relative w-full h-screen bg-sky-50 flex flex-col justify-between">
-            <Image
-                src="/background.svg"
-                layout="fill"
-                quality={100}
-                alt="Background"
-                priority
-            />
-            <div className="absolute bottom-20 right-0">
-                <Image
-                    src="/monkey-scooter.svg"
-                    width={500}
-                    height={500}
-                    objectFit="contain"
-                    alt="Monkey riding a scooter"
-                />
+        <div className=" bg-sky-50 ">
+            <div className="w-full h-screen relative bg-cover bg-no-repeat bg-bottom bg-[url('/background.svg')] ">
+                    <Image
+                        className="absolute top-[20%] right-0 max-w-[250px] w-100"
+                        src="/monkey-scooter.svg"
+                        width={500}
+                        height={500}
+                        objectFit="contain"
+                        alt="Monkey riding a scooter"
+                    />
             </div>
         </div>
     );
