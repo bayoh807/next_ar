6eb82eacdadbbf259d863a0f9df9e19fbcfab5f1
xiaoxu <innocent0705@gmail.com>
2024-09-07 23:43:00 +0800

    fix: update ARScene

--- components/ARScene.tsx
+++ components/ARScene.tsx
@@ -1,19 +1,19 @@
 "use client"
 
 import { ARCanvas, ARMarker } from '@artcom/react-three-arjs';
-import {useEffect,useState,useRef} from 'react';
-import { BoxGeometry } from 'three';
-import { extend } from '@react-three/fiber';
+import { useEffect, useState, useRef } from 'react';
+import { Box } from '@react-three/drei';
 import { useGLTF } from '@react-three/drei';
 import  ball  from  './../public/Ball.glb';
-import {useEffectfulState} from "@react-three/drei/helpers/useEffectfulState";
 
-extend({ BoxGeometry });
 
-const Model = ({ url } : {url : string}) =>{
+const Model = ({ url }: { url: string }) => {
     const { scene } = useGLTF(url);
-    return <primitive object={scene} scale={[1, 1, 1]} position={[0, 0, 0]}/>;
-}
+    useEffect(() => {
+    console.log('Model loaded', scene);
+  }, [scene]);
+  return <primitive object={scene} scale={[1, 1, 1]} position={[0, 0, 0]} />;
+};
 
 export default function ARScene() {
     const [isClient, setIsClient] = useState(true);
@@ -31,17 +31,25 @@ export default function ARScene() {
     }, []);
     return (
        isClient && (  <div >
-           <ARCanvas>
-               <ambientLight intensity={0.5} />
-               <directionalLight position={[0, 10, 5]} intensity={1} />
-               <ARMarker type="pattern" patternUrl="./patt.hiro">
-                   <mesh position={[0, 1, 0]} scale={[0.5, 0.5, 0.5]}>
-                       <boxGeometry args={[1, 1, 1]} />
-                       <meshBasicMaterial color="yellow" />
-                       <Model url={ball} />
-                   </mesh>
-               </ARMarker>
-           </ARCanvas>
+        <ARCanvas
+          onCreated={({ gl }) => {
+            console.log('ARCanvas created', gl);
+          }}
+        >
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[0, 10, 5]} intensity={1} />
+          <ARMarker
+            type="pattern"
+            patternUrl="./patt.hiro"
+            onMarkerFound={() => console.log('Marker found')}
+            onMarkerLost={() => console.log('Marker lost')}
+          >
+            <Box position={[0, 1, 0]} scale={[0.5, 0.5, 0.5]}>
+              <meshStandardMaterial attach="material" color="yellow" />
+            </Box>
+            <Model url={ball}/>
+          </ARMarker>
+        </ARCanvas>
        </div>)
     );
 }
